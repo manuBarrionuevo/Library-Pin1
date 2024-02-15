1334313cb4fe2acc31a6f3141c7a9b13c2a8b706
manuelBarrionuevo <manuelbarrionuevo678@hotmail.com>
2024-02-15 17:36:30 -0300

    update

--- vars/pinVars.groovy
+++ vars/pinVars.groovy
@@ -1,11 +1,12 @@
 def call() {
     def pinVars = [:]
 
-    pinVars.buildDockerImage = { imageName, version ->
-        sh """
-            docker build -t $imageName:$version .
-            docker images
-        """
+    pinVars.buildDockerImage = { imageName, version ->       
+            sh """
+                docker build -t $imageName:$version .
+                docker images
+            """
+        
     }
 
     pinVars.pushDockerImage = { imageName, version, directory ->
@@ -14,13 +15,14 @@ def call() {
         """
     }
 
-    pinVars.dockerLogin = {  ->
+    pinVars.dockerLogin = { registryUrl ->
         withCredentials([usernamePassword(credentialsId: 'dockerHub', usernameVariable: 'DOCKER_USER', passwordVariable: 'DOCKER_PASSWORD')]) {
-            sh  """
-                docker login -u $DOCKER_USER -p $DOCKER_PASSWORD
-            """
-            pinVarsInstance.buildDockerImage("${DOCKER_USER}/pin-1jenkins", "${version}")
+
+             withDockerRegistry([url: registryUrl]) {
+                return true
+            }
         }
+        return false
     }
 
     return pinVars
