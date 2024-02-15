1cbbdd0256162681d44cd2e8b82e7d8383c207b4
manuelBarrionuevo <manuelbarrionuevo678@hotmail.com>
2024-02-15 17:53:41 -0300

    update

--- vars/pinVars.groovy
+++ vars/pinVars.groovy
@@ -2,27 +2,32 @@ def call() {
     def pinVars = [:]
 
     pinVars.buildDockerImage = { imageName, version ->       
+        withDockerRegistry([url: 'https://registry.example.com']) {
             sh """
+                docker login -u $DOCKER_USER -p $DOCKER_PASSWORD
                 docker build -t $imageName:$version .
                 docker images
             """
-        
+        }
     }
 
     pinVars.pushDockerImage = { imageName, version, directory ->
-        sh """
-            docker push $imageName:$version
-        """
+        withDockerRegistry([url: 'https://registry.example.com']) {
+            sh """
+                docker login -u $DOCKER_USER -p $DOCKER_PASSWORD
+                docker push $imageName:$version
+            """
+        }
     }
 
     pinVars.dockerLogin = { registryUrl ->
         withCredentials([usernamePassword(credentialsId: 'dockerHub', usernameVariable: 'DOCKER_USER', passwordVariable: 'DOCKER_PASSWORD')]) {
-
-             withDockerRegistry([url: registryUrl]) {
-                return true
+            withDockerRegistry([url: registryUrl]) {
+                sh  """
+                    docker login -u $DOCKER_USER -p $DOCKER_PASSWORD
+                """
             }
         }
-        return false
     }
 
     return pinVars
